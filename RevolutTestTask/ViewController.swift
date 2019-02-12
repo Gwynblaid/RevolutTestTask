2bb78ea938a0a572f573729942edaf6d4d778175
Sergey Kharchenko <sergey.kharchenko@abbyy.com>
2019-02-12 13:28:49 +0300

    Initial Commit

---
+++ RevolutTestTask/ViewController.swift
@@ -0,0 +1,16 @@
+// Copyright (C) ABBYY (BIT Software), 1993-2019 . All rights reserved.
+// Автор: Sergey Kharchenko
+// Описание: @warning добавить описание
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
